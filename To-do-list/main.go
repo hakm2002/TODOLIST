8f9b9d2b3aadd9db403b20c71443cecc27c3a742
hakm2002 <141743277+hakm2002@users.noreply.github.com>
2026-01-13 14:40:18 +0700

    Update main.go

--- To-do-list/main.go
+++ To-do-list/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"www.github.com/2021-5-20/t_d_list/config"
-	"www.github.com/2021-5-20/t_d_list/routes"
+	
+	// PERHATIKAN BARIS INI: Sesuaikan dengan nama module baru di langkah 1
+	"github.com/hakm2002/TODOLIST/config" 
+	"github.com/hakm2002/TODOLIST/routes"
 )
 
 func main() {
+	// Init DB (Ingat untuk update file config/db.go agar pakai os.Getenv seperti panduan sebelumnya)
 	config.InitDB()
+	
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // 或者具体 origin
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
@@ -22,6 +26,9 @@ func main() {
 
 		c.Next()
 	})
+
 	routes.InitRoutes(r)
-	r.Run(":3000") // listen and serve on  (for windows "localhost:3000")
+	
+	// PENTING: Gunakan port 8080 agar sesuai dengan Jenkinsfile & Dockerfile
+	r.Run(":8080") 
 }
